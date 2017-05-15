0313078c9898fa0ef369082ec1606972c2adcd68
Eugene <evgen.voynilo@gmail.com>
2017-05-15 12:53:47 +0300

    Оформление

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,9 @@
 var link = document.querySelector(".feedback_link");
-
 var popup = document.querySelector(".feedback_form");
 var close = popup.querySelector(".feedback_close");
-
 var form = popup.querySelector(".form_button");
 var login = popup.querySelector("[name=name]");
 var mail = popup.querySelector("[name=mail]");
-
 var storage = localStorage.getItem(".feedback_link");
 
 link.addEventListener("click", function(event) {
@@ -43,12 +40,12 @@ function initialize() {
   var mapOptions = {
     scrollwheel: false,
     zoom: 17,
-    center: new google.maps.LatLng(59.9389678, 30.320000)
+    center: new google.maps.LatLng(59.93920802, 30.32150616)
   }
   var map = new google.maps.Map(document.getElementById("map"),
     mapOptions);
   var image = 'img/map-marker.png';
-  var LatLng = new google.maps.LatLng(59.9387100, 30.323800);
+  var LatLng = new google.maps.LatLng(59.9387942, 30.3230833);
   var beachMarker = new google.maps.Marker({
     position: LatLng,
     map: map,
